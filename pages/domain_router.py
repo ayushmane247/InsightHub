2dd523a925975dded0c51f58403ea2113610228e
Ayush Mane <ayushmane3036@gmail.com>
2025-07-23 23:59:16 +0530

    Delete pages directory

--- pages/domain_router.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def domain_router():
-    domain = st.sidebar.selectbox("Choose Domain", ["Stores", "Games", "Movies"])
-    st.session_state['domain'] = domain
-    st.success(f"You selected: {domain}")
-
-if __name__ == "__main__":
-    domain_router()
